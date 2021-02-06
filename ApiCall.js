20526598655594d201a471ab93a85ed65427fd80
MeineQuest <daniel.eichler07@Gmail.com>
2021-02-06 20:09:32 +0100

    added check for responseCode in ApiCall

--- ApiCall.js
+++ ApiCall.js
@@ -1,10 +1,17 @@
 function apiCall(url) {
   try {
-      var response = UrlFetchApp.fetch(url, {followRedirects: false});
-    return response; 
-  } catch (e) {
+    var response = UrlFetchApp.fetch(url, { followRedirects: false });
+    var responseCode = response.getResponseCode()
 
+    if (responseCode === 200) {
+      Logger.log("ResponseCode " + response.getResponseCode());
+      return response;
+    } else {
+      Logger.log("Failure ResponseCode " + response.getResponseCode());
+    };
+  }
+  catch (e) {
     Logger.log(e)
-    return "";
+    Logger.log(response)
   }
 }
